6633488d4dbb288ab0eb1ef8302e74e9b3277524
長谷部亮 <wtc.r.h173@gmail.com>
2017-11-29 12:30:19 +0900

    validate/ profile_image

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :user
   has_many :relationships, dependent: :destroy
   has_many :relationship_users, through: :relationships, source: :user
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
